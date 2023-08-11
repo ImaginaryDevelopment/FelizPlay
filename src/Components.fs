c4068da453e4744c86046ea2ecc542799669a993
ImaginaryDevelopment <bdimpe@gmail>
2023-08-10 18:21:35 -0700

    learning router, effects

--- src/Components.fs
+++ src/Components.fs
@@ -17,11 +17,30 @@ type Components =
     [<ReactComponent>]
     static member Counter() =
         let (count, setCount) = React.useState(0)
+        if false then // https://github.com/Zaid-Ajaj/Feliz/blob/master/public/Feliz/React/EffectfulComponents.md
+            React.useEffect(fun () ->
+                Browser.Dom.document.title <- sprintf "Count = %d" count
+            )
         Html.div [
             Html.h1 count
             Html.button [
                 prop.onClick (fun _ -> setCount(count + 1))
                 prop.text "Increment"
+            ]
+        ]
+    static member Index(updateUrl) =
+        Html.div [
+            Html.h1 "Index"
+            Html.ul [
+                Html.li [
+                    Html.a [ 
+                        prop.href "counter"
+                        prop.text "Counter"
+                        prop.onClick(fun e ->
+                            e.preventDefault()
+                            updateUrl "counter")
+                    ]
+                ]
             ]
         ]
 
@@ -31,14 +50,24 @@ type Components =
     /// </summary>
     [<ReactComponent>]
     static member Router() =
-        let (currentUrl, updateUrl) = React.useState(Router.currentUrl())
+        let (currentUrl, updateUrl) =
+            React.useState(Router.currentUrl())
         React.router [
             router.onUrlChanged updateUrl
             router.children [
+                printfn "currentUrl:%A" currentUrl
                 match currentUrl with
-                | [ ] -> Html.h1 "Index"
-                | [ "hello" ] -> Components.HelloWorld()
-                | [ "counter" ] -> Components.Counter()
+                | [ ] ->
+                    printfn "Router: Index"
+                    Components.Index(fun next ->
+                        updateUrl (next::currentUrl)
+                    )
+                | [ "hello" ] ->
+                    printfn "Router: Hello"
+                    Components.HelloWorld()
+                | [ "counter" ] ->
+                    printfn "Router: Counter"
+                    Components.Counter()
                 | otherwise -> Html.h1 "Not found"
             ]
         ]
